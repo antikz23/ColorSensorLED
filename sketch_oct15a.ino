1fa791c4882597e6a850c72f5e96671a15937b08
antikz23 <jslizzz30@gmail.com>
2018-10-15 14:05:58 -0400

    Add files via upload

---
+++ sketch_oct15a.ino
@@ -0,0 +1,23 @@
+int x;
+
+void setup()
+{
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(8, INPUT);
+}
+
+void loop()
+{
+
+//red = red >> 8;
+  for (int y = 0; y < 255; y++) {
+    analogWrite(9, y >> 8);
+    analogWrite(10, y >> 8);
+    analogWrite(11, y >> 8);
+    delay(40);
+  }
+  x = analogRead(8);
+
+}

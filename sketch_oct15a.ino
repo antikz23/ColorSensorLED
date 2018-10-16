44f7545978c33f8540a85f233c40441fdd5df5e7
antikz23 <jslizzz30@gmail.com>
2018-10-16 13:57:38 -0400

    Add files via upload

--- sketch_oct15a.ino
+++ sketch_oct15a.ino
@@ -11,11 +11,14 @@ void setup()
 void loop()
 {
 
-//red = red >> 8;
+  //red = red >> 8;
   for (int y = 0; y < 255; y++) {
-    analogWrite(9, y >> 8);
-    analogWrite(10, y >> 8);
-    analogWrite(11, y >> 8);
+    analogWrite(9, y);
+    delay(50);
+    analogWrite(10, y);
+    delay(50);
+    analogWrite(11, y);
+    delay(50);
     delay(40);
   }
   x = analogRead(8);
